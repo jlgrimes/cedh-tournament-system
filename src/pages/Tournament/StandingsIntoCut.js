868e971d6e85915ffd3a2311ab4ff28700df594a
Jared Grimes <jaredlgrimes@hotmail.com>
2021-08-22 14:43:59 -0400

    generates standings

--- src/pages/Tournament/StandingsIntoCut.js
+++ src/pages/Tournament/StandingsIntoCut.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { List, ListItem, ListItemText } from '@material-ui/core';
-
 import { useSelector } from 'react-redux';
+import orderBy from 'lodash/orderBy';
+
+import { getTotalPoints } from '../../utils/pairing';
 
 const StandingsIntoCut = () => {
-  const standings = useSelector((state) => state.tournament.players);
+  const players = useSelector((state) => state.tournament.players)
+  const standings = orderBy(players, (player) => getTotalPoints(player), 'desc');
   console.log(standings);
 
   return (
     <div>
-      {standings}
+      <List>
+        {standings.map((player) => (
+          <ListItem>
+            <ListItemText>
+              {`${player.name} - ${getTotalPoints(player)} points`}
+            </ListItemText>
+          </ListItem>
+        ))}
+      </List>
     </div>
   )
 };
